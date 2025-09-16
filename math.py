eb17db627004b22332c408b354852b3729700a02
Afsa8257 <afs8257@g.rit.edu>
2025-09-16 09:12:28 +0400

    This file has been added.

---
+++ math.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    return x + y
+
+def sub(x,y):
+    return x-y
+
+def mul(x,y):
+    return x * y
+
+def div(x,y):
+    return x/y
+
+def main():
+    x = int(input("Enter the value of x: "))
+    y = int(input("Enter the value of y: "))
+    print("Addition:", add(x,y))
+    print("Subtraction:", sub(x,y))
+    print("Multiplication:", mul(x,y))
+    print("Division:", div(x,y))
+main()
