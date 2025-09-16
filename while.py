26e373207164202407387a2128bc3afb5702c8e6
Afsa8257 <afs8257@g.rit.edu>
2025-09-16 11:02:34 +0400

    while.py has been added

---
+++ while.py
@@ -0,0 +1,23 @@
+#Print even numbers using while loop
+i = 2
+while i<=10:
+    print(i)
+    i+=2 #same as i = i+1 (Incrementing)
+
+#Print odd numbers using while loop
+j = 1
+while j<=10:
+    print(j)
+    j+=2
+
+#Print numbers from 10-1 using while loop
+k = 10
+while k>=1:
+    print(k)
+    k -= 1 #same as i = i-1 (Decrementing)
+
+#Print squares of numbers from 1-10
+x = 1
+while x<=10:
+    print(x**3)
+    x+=1
