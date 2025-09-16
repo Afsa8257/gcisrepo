230e826c5bac20b8fa9dc8c50c1e4a8c24c198d5
Afsa8257 <afs8257@g.rit.edu>
2025-09-16 09:13:03 +0400

    This if file has been added.

---
+++ if.py
@@ -0,0 +1,15 @@
+num=int(input("Enter your input: "))
+
+
+if num>0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
+
+num1=int(input("Enter your input: "))
+if num1%2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+
