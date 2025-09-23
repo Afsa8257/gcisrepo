0f9a8fe50fc63e5cec0492d5d4c8b8154cc1ac06
Afsa8257 <afs8257@g.rit.edu>
2025-09-23 10:53:36 +0400

    check.py has been added.

--- check.py
+++ check.py
@@ -1,15 +1,28 @@
 def even_or_odd(number):
-    if number%2==0:
+    '''
+    This function checks if the number is even or odd.
+    If the remainder is zero when the given number is divided by 2, the given number is even. 
+    Otherwise it's odd.
+    '''
+    if number%2==0: #Uses mod function
         print("This is an even number.")
     else:
         print("This is an odd number.")
     
 def square_cube(integer):
-    print("Square of the number=",integer**2)
-    print("Cube of the number=",integer**3)
+    '''
+    This function displays the square and cube of the given number.
+    '''
+    print("Square of the number=",integer**2) # Gives the square of the number
+    print("Cube of the number=",integer**3) # Gives the cube of the number
 
 def main():
-    input_user=int(input("Enter a number: "))
-    even_or_odd(input_user)
-    square_cube(input_user)
-main()
+    '''
+    Takes an input from the user.
+    Checks if the input is even or odd and displays if the input is even or odd.
+    Displays the square and cube of the input.
+    '''
+    input_user=int(input("Enter a number: ")) # Takes an input using input and int functions.
+    even_or_odd(input_user) # Calls the even_or_odd function and inputs the user's input.
+    square_cube(input_user) # Calls the square_cube function and inputs the user's input.
+main() # Calls main function
