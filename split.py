8a454e50541ea182ed40b1f5d092f7ed8ffd7d4f
Afsa8257 <afs8257@g.rit.edu>
2025-09-25 09:29:42 +0400

    This split file has been added.

---
+++ split.py
@@ -0,0 +1,12 @@
+a_string = "This is a sentence."
+tokens = a_string.split(" ")
+print(tokens[1]) #11
+for i in tokens:
+    print(i) 
+    '''
+    Displays:
+    This
+    is
+    a
+    sentence.
+    '''
